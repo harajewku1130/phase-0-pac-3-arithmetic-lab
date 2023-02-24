5caecbf6e8de0f67228eb61ecfcb251258b1ed04
+ <+>
2023-02-23 18:11:59 -0600

    Completed the lab

---
+++ index.js
@@ -0,0 +1,26 @@
+function add (num1, num2) {
+    return num1 + num2;
+}
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+function divide (num1, num2) {
+    return num1 / num2;
+}
+function increment(n) {
+   n++;
+   return n;
+}
+function decrement(n) {
+    n--;
+    return n;
+ }
+ function makeInt(string){
+    return parseInt(string, 10);
+ }
+ function preserveDecimal(string){
+    return parseFloat(string);
+ }
